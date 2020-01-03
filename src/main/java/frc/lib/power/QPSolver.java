488601d7761c859a066b448825df616d3820352b
cttdev <cttewari@gmail.com>
2020-01-03 13:16:58 -0800

    Adding more constraints.

--- src/main/java/frc/lib/power/QPSolver.java
+++ src/main/java/frc/lib/power/QPSolver.java
@@ -7,20 +7,23 @@ import com.cureos.numerics.CobylaExitStatus;
 public class QPSolver {
 
     public static void main(String[] args) {
-        double rhobeg = 0.5;
+        double rhobeg = 0.01;
         double rhoend = 1.0e-6;
         int iprint = 1;
-        int maxfun = 1000;
+        int maxfun = 150;
 
         Calcfc calcfc = new Calcfc() {
             @Override
             public double Compute(int n, int m, double[] x, double[] con) {
                 con[0] = 120 - (140 * x[0]) + (30 * x[1]);
+                con[1] = 1 - x[0];
+                con[2] = 1 - x[1];
                 return -0.3 * Math.pow(x[0], 2.0) + -0.6 * Math.pow(x[1], 2.0);
             }
         };
         double[] x = {0.0, 0.0};
-        CobylaExitStatus result = Cobyla.FindMinimum(calcfc, 2, 1, x, rhobeg, rhoend, iprint, maxfun);
+        CobylaExitStatus result = Cobyla.FindMinimum(calcfc, 2, 3, x, rhobeg, rhoend, iprint, maxfun);
         System.out.println(x);
+
     }
 }
