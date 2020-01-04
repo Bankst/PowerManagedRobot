452dd1411bd72e864d545213a80d903f0a566911
cttdev <cttewari@gmail.com>
2020-01-03 20:12:27 -0800

    Didn't distribute a negative.

--- src/main/java/frc/lib/power/QPSolver.java
+++ src/main/java/frc/lib/power/QPSolver.java
@@ -7,15 +7,15 @@ import com.cureos.numerics.CobylaExitStatus;
 public class QPSolver {
 
     public static void main(String[] args) {
-        double rhobeg = 0.01;
-        double rhoend = 1.0e-6;
+        double rhobeg = 0.1;
+        double rhoend = 1.0e-10;
         int iprint = 1;
-        int maxfun = 150;
+        int maxfun = 100;
 
         Calcfc calcfc = new Calcfc() {
             @Override
             public double Compute(int n, int m, double[] x, double[] con) {
-                con[0] = 120 - (140 * x[0]) + (30 * x[1]);
+                con[0] = 120 - ((140 * x[0]) + (100 * x[1]));
                 con[1] = 1 - x[0];
                 con[2] = 1 - x[1];
                 return -0.3 * Math.pow(x[0], 2.0) + -0.6 * Math.pow(x[1], 2.0);
